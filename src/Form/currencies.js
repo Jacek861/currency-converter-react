702017b95e4dc73109ba9437f5f0267a56b97fc4
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-03-16 11:55:48 +0000

    currencies array created

---
+++ src/Form/currencies.js
@@ -0,0 +1,28 @@
+export const currencies = [
+    {
+        shortName: "GBP",
+        longName: "British Pound",
+        rate: "5.37",
+    },
+    {
+        shortName: "USD",
+        longName: "US Dolar",
+        rate: "4.35",
+    },
+    {
+        shortName: "EUR",
+        longName: "Euro",
+        rate: "4.71",
+    },
+    {
+        shortName: "AUD",
+        longName: "Australian Dolar",
+        rate: "3.01",
+    },
+    {
+        shortName: "CAD",
+        longName: "Canadian Dolar",
+        rate: "3.23",
+    },
+]
+
