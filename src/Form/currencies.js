0abaa1c7a1bc2ca06284fa005c626cc59684afa9
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-03-17 12:33:35 +0000

    currencies update

--- src/Form/currencies.js
+++ src/Form/currencies.js
@@ -2,27 +2,27 @@ export const currencies = [
     {
         shortName: "GBP",
         longName: "British Pound",
-        rate: "5.37",
+        rate: 5.37
     },
     {
         shortName: "USD",
         longName: "US Dolar",
-        rate: "4.35",
+        rate: 4.35
     },
     {
         shortName: "EUR",
         longName: "Euro",
-        rate: "4.71",
+        rate: 4.71
     },
     {
         shortName: "AUD",
         longName: "Australian Dolar",
-        rate: "3.01",
+        rate: 3.01
     },
     {
         shortName: "CAD",
         longName: "Canadian Dolar",
-        rate: "3.23",
+        rate: 3.23
     },
-]
+];
 
