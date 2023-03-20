f6a0e6c081d123d6436825a471601de1baf5c996
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-03-20 21:35:08 +0000

    result compnent update

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -4,9 +4,9 @@ export const Result = ({ result }) => (
   <p className="form__result">
     {result !== undefined && (
       <>
-        {result.basicAmount}&nbsp;PLN&nbsp;={" "}
+        {result.basicAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
         <strong>
-          {result.finalAmount}&nbsp;{result.currency}
+          {result.finalAmount.toFixed(2)}&nbsp;{result.currency}
         </strong>
       </>
     )}
