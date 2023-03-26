c777085a821aa88b0917c625b371d022fa17080c
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-03-26 15:59:15 +0100

    className in Result component changed

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,10 +1,11 @@
 import "./style.css";
 
 export const Result = ({ result }) => (
-  <p className="form__result">
+  <p className="result">
     {result !== undefined && (
       <>
-        {result.basicAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
+        {result.basicAmount.toFixed(2)}&nbsp;PLN&nbsp;= {" "}
+
         <strong>
           {result.finalAmount.toFixed(2)}&nbsp;{result.currency}
         </strong>
