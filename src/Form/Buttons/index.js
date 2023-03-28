eb59de58591f358b85ee42a26b35d0faca9d0d38
Jacek Opalinski <jacek.opalinski861@gmail.com>
2023-03-28 16:53:41 +0100

    styled component in Buttons implemented

--- src/Form/Buttons/index.js
+++ src/Form/Buttons/index.js
@@ -1,34 +1,4 @@
-
-import styled from "styled-components";
-
-const DivButtons = styled.divButtons`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    justify-items: center;
-
-    @media (max-width: 767px) {
-        grid-template-columns: 1fr;
-        grid-gap: 20px;
-    }
-`;
-
-const Button = styled.button`
-    width: 60%;
-    border: none;
-    background-color: lightslategrey;
-    border-radius: 5px;
-    padding: 10px;
-    cursor: pointer;
-    font-weight: 600;
-    box-shadow: 2px 2px 10px black;
-    transition: background 2s, transform 0.5s;
-
-    &:hover {
-        color: brown;
-        background-color: rgb(255, 255, 255);
-        transform: scale(1.1)
-    }
-`;
+import { DivButtons, Button } from "./styled";
 
 export const Buttons = ({ onResetClick }) => (
     <DivButtons>
